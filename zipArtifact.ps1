e6b41a97ea9560ec755f04c43ccfc5a8a2946392
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-16 19:56:20 +0100

    Updated zipArtifact.ps1

--- zipArtifact.ps1
+++ zipArtifact.ps1
@@ -1,10 +1,9 @@
 Remove-Item -path "./target/jdbc_drivers" -Recurse -Include mssql*.jar
-Get-ChildItem -path "./target/" -Recurse -Include DBTestCompare-*SNAPSHOT-jar-with-dependencies.jar | compress-archive -DestinationPath ./DBTestCompare$(dBTestCompareVersion).zip
-compress-archive -path "./test-definitions" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive -path "./deploy" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive -path "./target/jdbc_drivers" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive -path "./target/jdbc_drivers" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive "README.md" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive "LICENSE" DBTestCompare$(dBTestCompareVersion).zip -update
-compress-archive "LICENSE-3RD-PARTY" DBTestCompare$(dBTestCompareVersion).zip -update
+Get-ChildItem -path "./target/" -Recurse -Include DBTestCompare-*SNAPSHOT-jar-with-dependencies.jar | compress-archive -DestinationPath ./DBTestCompare.zip
+compress-archive -path "./test-definitions" DBTestCompare.zip -update
+compress-archive -path "./deploy" DBTestCompare.zip -update
+compress-archive -path "./target/jdbc_drivers" DBTestCompare.zip -update
+compress-archive "README.md" DBTestCompare.zip -update
+compress-archive "LICENSE" DBTestCompare.zip -update
+compress-archive "LICENSE-3RD-PARTY" DBTestCompare.zip -update
 
