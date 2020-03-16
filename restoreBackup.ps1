b4e8e256cd9a322e1d7324ff1bd6bc35b8b31c86
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-16 15:37:54 +0100

    Updated restoreBackup.ps1

--- restoreBackup.ps1
+++ restoreBackup.ps1
@@ -3,3 +3,5 @@ echo "mkdir /var/opt/mssql/backup"
 docker exec -i "sqlserver-container" mkdir "/var/opt/mssql/backup"
 echo "copy ./docker/AdventureWorks2008R2FullDatabaseBackup.bak to /var/opt/mssql/backup" 
 docker cp "./docker/AdventureWorks2008R2FullDatabaseBackup.bak" sqlserver-container:/var/opt/mssql/backup
+echo "restore backup"
+docker exec -i sqlserver-container /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "yourStrong22Password" -Q "RESTORE DATABASE AdventureWorks2008R2 FROM DISK = '/var/opt/mssql/backup/AdventureWorks2008R2FullDatabaseBackup.bak' WITH MOVE 'AdventureWorks2008R2_Data' TO '/var/opt/mssql/data/AdventureWorks2008R2.mdf', MOVE 'AdventureWorks2008R2_Log' TO '/var/opt/mssql/data/AdventureWorks2008R2_1.LDF'"
