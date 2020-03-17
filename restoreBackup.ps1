f1cc6dc811a6afb3f5126680400d1f22eec1645c
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-17 19:27:34 +0100

    Added HEALTHCHECK for sqlserver

--- restoreBackup.ps1
+++ restoreBackup.ps1
@@ -8,5 +8,7 @@ do
 {
 	$JSON=docker inspect --format='{{json .State.Health}}'  sqlserver-container |  Out-String | ConvertFrom-Json
 	echo $JSON.Status.ToString()
-}Until ($JSON.Status.ToString() -eq 'healthy')
+	Start-Sleep -s 10
+}
+Until ($JSON.Status.ToString() -eq 'healthy')
 docker exec -i sqlserver-container /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "yourStrong22Password" -Q "RESTORE DATABASE AdventureWorks2008R2 FROM DISK = '/var/opt/mssql/backup/AdventureWorks2008R2FullDatabaseBackup.bak' WITH MOVE 'AdventureWorks2008R2_Data' TO '/var/opt/mssql/data/AdventureWorks2008R2.mdf', MOVE 'AdventureWorks2008R2_Log' TO '/var/opt/mssql/data/AdventureWorks2008R2_1.LDF'"
