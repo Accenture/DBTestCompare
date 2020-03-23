3187d1e9c6430d663d0f0d920ea4472c94d491f0
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-23 10:09:59 +0100

    Updated variables

--- setDBTestCompareVersion.ps1
+++ setDBTestCompareVersion.ps1
@@ -1,3 +1,3 @@
-Write-Output "Setting  DBTestCompere version $env:dBTestCompareVersion"
+Write-Output "Setting  DBTestCompere version " $env:dBTestCompareVersion
 $version="<version>$env:dBTestCompareVersion-SNAPSHOT</version>"
 ((Get-Content -path ./pom.xml -Raw) -replace '<version>\d\.\d-SNAPSHOT<\/version>',$version) | Set-Content -Path ./pom.xml
