6c916fe9781812af54f4544a7cd1d230ded32ab1
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-24 07:37:12 +0100

    Added zipFileTestResults to logs

--- zipArtifact.ps1
+++ zipArtifact.ps1
@@ -1,7 +1,8 @@
 $zipFile="./zip/DBTestCompare$env:DBTESTCOMPAREVERSION.zip"
 $zipFileTestResults="./zip/DBTestCompare$env:DBTESTCOMPAREVERSION.TestResults.$env:BUILD_BUILDNUMBER.zip"
 New-Item -Path './zip' -ItemType Directory -Force
-Write-Output "Artifact file name $zipFile"
+Write-Output "Artifact application file name $zipFile"
+Write-Output "Artifact test results file name $zipFileTestResults"
 Remove-Item -path "./target/jdbc_drivers" -Recurse -Include mssql*.jar
 Get-ChildItem -path "./target/" -Recurse -Include DBTestCompare-*SNAPSHOT-jar-with-dependencies.jar `
     | compress-archive -DestinationPath ./$zipFile
@@ -11,4 +12,4 @@ compress-archive -path "./target/jdbc_drivers" $zipFile -update
 compress-archive "README.md" $zipFile -update
 compress-archive "LICENSE" $zipFile -update
 compress-archive "LICENSE-3RD-PARTY" $zipFile -update
-compress-archive -path "./target/test-output" $zipFileTestResults -update
+compress-archive -path "./target/test-output" $zipFileTestResults
