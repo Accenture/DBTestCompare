77312c17107cf9676affe12122fbe819c6028e51
Jakub Raczek <jraczek@objectivity.co.uk>
2020-03-24 08:14:15 +0100

    Fixed definition of test-definitions/fetch_compare_test/from_file_sql_MySQL_PostgreSQL.xml

--- test-definitions/fetch_compare_test/sql1.sql
+++ test-definitions/fetch_compare_test/sql1.sql
@@ -1,7 +1,7 @@
-SELECT `personaddress`.`AddressID`,
-    `personaddress`.`AddressLine1`,
-    `personaddress`.`City`,
-    `personaddress`.`StateProvinceID`,
-    `personaddress`.`PostalCode`,
-    `personaddress`.`ModifiedDate`
-FROM `dbquality`.`personaddress`;
+SELECT personaddress.AddressID,
+    personaddress.AddressLine1,
+    personaddress.City,
+    personaddress.StateProvinceID,
+    personaddress.PostalCode,
+    personaddress.ModifiedDate
+FROM dbquality.personaddress;
