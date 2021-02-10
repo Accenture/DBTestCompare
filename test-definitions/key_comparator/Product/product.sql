d1d462a0c5262e6bb08236b35cc839b4c81f38bc
Jakub Raczek <jraczek@objectivity.co.uk>
2021-02-10 21:38:10 +0100

    Add Product.xml test

--- test-definitions/key_comparator/Product/product.sql
+++ test-definitions/key_comparator/Product/product.sql
@@ -2,7 +2,7 @@
       pr.[StoreNumber]
       ,pr.ProductID
       ,pr.[Date]
-      ,sum(pr.[Quantity])
+      ,pr.[Quantity]
   FROM [dbo].[TemplateKeyComparatorProducts] pr
-  inner join AdventureWorks2008R2.dbo.TemplateKeyComparatorCheckAggregators d on d.[StoreNumber]=pr.[StoreNumber] and d.ProductID=pr.ProductID
-  group by pr.[StoreNumber],pr.ProductID,pr.Date
+  inner join AdventureWorks2008R2.dbo.TemplateKeyComparatorCheckAggregators d 
+  on d.[StoreNumber]=pr.[StoreNumber] and d.ProductID=pr.ProductID
