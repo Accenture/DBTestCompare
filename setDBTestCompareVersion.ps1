2f4da12f712de585efddddf26a21a0681ad3a0be
Maciej Antosiak <mantosiak@objectivity.co.uk>
2024-02-19 11:44:08 +0100

    testing global var

--- setDBTestCompareVersion.ps1
+++ setDBTestCompareVersion.ps1
@@ -1,3 +1,1 @@
-Write-Output "Setting  DBTestCompare version " $env:DBTESTCOMPAREVERSION
-$version="<version>$env:DBTESTCOMPAREVERSION-SNAPSHOT</version>"
-((Get-Content -path ./pom.xml -Raw) -replace '<version>\d\.\d.\d-SNAPSHOT<\/version>',$version) | Set-Content -Path ./pom.xml
+Write-Output "Setting  DBTestCompare version " $env:dBTestCompareVersion
