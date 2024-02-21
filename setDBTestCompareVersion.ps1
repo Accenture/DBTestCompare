5e6fa4038a25d8ec4dd3bd704389d97e8fe17244
Maciej Antosiak <mantosiak@objectivity.co.uk>
2024-02-21 09:08:49 +0100

    reverting changes in setDBTestCompareVersion.ps1

--- setDBTestCompareVersion.ps1
+++ setDBTestCompareVersion.ps1
@@ -1,1 +1,3 @@
-Write-Output "Setting  DBTestCompare version " $env:dBTestCompareVersion
+Write-Output "Setting  DBTestCompare version " $env:DBTESTCOMPAREVERSION
+$version="<version>$env:DBTESTCOMPAREVERSION-SNAPSHOT</version>"
+((Get-Content -path ./pom.xml -Raw) -replace '<version>\d\.\d.\d-SNAPSHOT<\/version>',$version) | Set-Content -Path ./pom.xml
